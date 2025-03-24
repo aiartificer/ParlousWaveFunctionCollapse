1cc7a91230db95a1ba94201a592f4a99004e083e
aiartificer <aiartificer@syntheticsapien.com>
2025-03-23 21:07:49 -0400

    Further fleshed out circle unit tests and added pretty print helper function that offsets the hex map

--- test/test_parlous_array.lua
+++ test/test_parlous_array.lua
@@ -26,6 +26,27 @@ function Test_Parlous_WFC_Foreach()
   assert(sum == 55, "Expected sum=55, actually sum="..tostring(sum))
 end
 
+function PPPrint(hex_map)
+  hex_map:foreach(
+    function (i, x)
+      if i % 10 == 0 then io.write('\n') end
+      if x ~= 0 then io.write(hex_map.bit(-x - 1)..'\t')  -- math.sqrt
+      else io.write('.\t') end
+    end)
+  print()
+end
+
+function Even_PPPrint(hex_map, WIDTH)
+  hex_map:foreach(
+    function (i, x)
+      if i % WIDTH == 0 then io.write('\n\n') end
+      if i % WIDTH == 0 and math.floor(i/WIDTH)%2 == 0 then io.write('  ') end
+      if x ~= 0 then io.write(hex_map.bit(-x - 1)..'    ')
+      else io.write('.    ') end
+    end)
+  print()
+end
+
 function PPrint_Hex_Map(hex_map)
   for i=1, #hex_map do
     print("hex_map["..tostring(i-1).."] = "..tostring(hex_map[i-1]))
@@ -136,6 +157,7 @@ end
 function Test_Circle_Placement()
   print("\n\nTest_Circle_Placement")
   local hex_map = Setup_Hex_Map(100, 8)
+  hex_map:map(function (x) return 0 end)
   local idx = 0
   local found = 0
   local foundAny = 0
@@ -145,18 +167,20 @@ function Test_Circle_Placement()
     idx = idx + 1
     return 2^((idx-1)%120)
   end)
-  hex_map:foreach(
-    function (i, x)
-      if i % 10 == 0 then io.write('\n') end
-      if x ~= 0 then io.write(hex_map.bit(-x - 1)..'\t')  -- math.sqrt
-      else io.write('.\t') end
-    end)
-  print()  -- ### DEBUG
+  Even_PPPrint(hex_map, 10)
+  -- hex_map:foreach(
+  --   function (i, x)
+  --     if i % 10 == 0 then io.write('\n') end
+  --     if x ~= 0 then io.write(hex_map.bit(-x - 1)..'\t')  -- math.sqrt
+  --     else io.write('.\t') end
+  --   end)
+  -- print()  -- ### DEBUG
 end
 
 function Test_HexMapHelper_function()
   print("\n\nTest_HexMapHelper_function")
   local hex_map = Setup_Hex_Map(100, 8)
+  hex_map:map(function (x) return 0 end)
   local idx = 0
   local found1 = 0
   local found2 = 0
@@ -169,55 +193,131 @@ function Test_HexMapHelper_function()
     idx = idx + 1
     if hx(-1,0) == 1 then found1 = idx end
     if hx(0,-1) == 2 then found2 = idx end
-    if hx(1,-1) == 128 then found3 = idx end
-    if hx(1,0) == 32 then found4 = idx end
-    if hx(0,1) == 128 then found5 = idx end
+    if hx(1,-1) == 128 then found3 = idx end -- 2^7: 8
+    if hx(1,0) == 32 then found4 = idx end -- 2^5: 6
+    if hx(0,1) == 128 then found5 = idx end -- 2^7: 8
     if hx(-1,1) == 4 then found6 = idx end
     -- io.write('['..idx..': '..hex_map.bit(hx(-1,0))..'], ')  -- ### DEBUG
     return 2^((idx-1)%120)
   end)
-  hex_map:foreach(
-    function (i, x)
-      if i % 10 == 0 then io.write('\n') end
-      if x ~= 0 then io.write(hex_map.bit(-x - 1)..'\t')  -- math.sqrt
-      else io.write('.\t') end
-    end)
+  Even_PPPrint(hex_map, 10)
+  -- PPPrint(hex_map)
+  -- hex_map:foreach(
+  --   function (i, x)
+  --     if i % 10 == 0 then io.write('\n') end
+  --     if x ~= 0 then io.write(hex_map.bit(-x - 1)..'\t')  -- math.sqrt
+  --     else io.write('.\t') end
+  --   end)
   -- print()
   assert(found1 == 17, "Expected value of 17, actually "..tostring(found1))
-  assert(found2 == 24, "Expected value of 24, actually "..tostring(found2))
+  assert(found2 == 36, "Expected value of 36, actually "..tostring(found2))
   assert(found3 == 9, "Expected value of 9, actually "..tostring(found3))
   assert(found4 == 17, "Expected value of 17, actually "..tostring(found4))
   assert(found5 == 21, "Expected value of 21, actually "..tostring(found5))
-  assert(found6 == 28, "Expected value of 28, actually "..tostring(found6))
+  assert(found6 == 26, "Expected value of 26, actually "..tostring(found6))
 end
 
 function Test_Circle()
   print("\n\nTest_Circle")
-  local hex_map = Setup_Hex_Map(100, 8)
+  print("----------------------------------------")
+  local WIDTH = 12 -- 20
+
+  -- Initialize hex_map
+  local hex_map = parlous_wfc.new_hex_map(WIDTH*WIDTH, 8, WIDTH, 5, 17)
   local idx = 0
-  local found = 0
-  local foundAny = 0
-  local foundAll = 0
+  hex_map:map(function (x)
+    idx = idx + 1
+    if idx == math.floor(WIDTH*WIDTH/2) + math.floor(WIDTH/2) then return hex_map.Not(2^7) end
+    return 0 end)
+  -- PPPrint(hex_map)
+
+  -- Define function to look for set hex cell (bit = 8)s
+  function Look_for_8(v, bit_count, bit)
+    if bit_count(v) ~= 1 then return false end
+    if bit(v) == 8 then return true else return false end
+   end
+
+  -- Generate map listing circle distances from set hex cell
+  idx = 0
+  local repeats = 0
   hex_map:gen(
   function (hx, x, newWave, dist, cir)
     idx = idx + 1
-    -- io.write('['..idx..': '..hex_map.bit(cir(1)[0])..'], ')  -- ### DEBUG
-    if cir(1)[0] == 128 then found = idx end
-    if cir(1):any(function(v, bit_count, bit)
-      assert(bit_count(v) >= 0, "Expected value at least 0, actually "..tostring(bit_count(v)))
-      return v == 128 end) then foundAny = idx end
-    if cir(1):all(function(v, bit_count, bit)
-      assert(bit_count(v) >= 0, "Expected value at least 0, actually "..tostring(bit_count(v)))
-      return v == 128 end) then foundAll = idx end
-    return 2^((idx-1)%120)
+    if hex_map.bit_count(x) == 1 then repeats = repeats + 1 end
+    if cir(1):any(Look_for_8) then return 1 end
+    if cir(2):any(Look_for_8) then return 2 end
+    if cir(3):any(Look_for_8) then return 4 end
+    if cir(4):any(Look_for_8) then return 8 end
+    if cir(5):any(Look_for_8) then return 16 end
+    return 32
   end)
-  -- print()  -- FIXME Assertions fail without this print statement!!!
-  print("found = "..found)  -- ### DEBUG
-  print("foundAny = "..foundAny)  -- ### DEBUG
-  print("foundAll = "..foundAll)  -- ### DEBUG
-  assert(found > 0, "Expected value greater than 0, actually "..tostring(found))
-  assert(foundAny > 0, "Expected value greater than 0, actually "..tostring(foundAny))
-  assert(foundAll == 0, "Expected value equal to 0, actually "..tostring(foundAll))
+  hex_map:foreach(
+    function (i, x)
+      if i % WIDTH == 0 then io.write('\n\n') end
+      if i % WIDTH == 0 and math.floor(i/WIDTH)%2 == 0 then io.write('  ') end
+      if x ~= 0 then io.write(hex_map.bit(-x - 1)..'    ')
+      else io.write('.    ') end
+    end)
+  print()
+  assert(repeats == 0, "Expected value of 0, actually "..tostring(repeats))
+end
+
+function Test_Circle_Any()
+  print("\n\nTest_Circle_Any")
+  local hex_map = Setup_Hex_Map(100, 8)
+  hex_map:map(function (x) return 0 end)
+  local idx = 0
+  local foundAny1 = 0
+  local foundAny2 = 0
+  local foundAny3 = 0
+  hex_map:gen(function (hx, x, newWave, dist, cir)
+    idx = idx + 1
+    if idx == 8 then return 8 end
+    if cir(1):any(function(v, bit_count, bit)
+      if bit_count(v) ~= 1 then return false end
+      -- print("bit(v) = "..bit(v).."\tv = "..v)  -- ### DEBUG
+      if bit(v) == 4 then return true else return false end
+    end) then foundAny1 = foundAny1 + 1 end
+    if cir(2):any(function(v, bit_count, bit)
+      if bit_count(v) ~= 1 then return false end
+      if bit(v) == 4 then return true else return false end
+    end) then foundAny2 = foundAny2 + 1 end
+    if cir(3):any(function(v, bit_count, bit)
+      if bit_count(v) ~= 1 then return false end
+      -- if bit(v) == 4 then print("["..idx.."]:\tbit(v) = "..bit(v).."\tv = "..v) end  -- ### DEBUG
+      if bit(v) == 4 then return true else return false end
+    end) then foundAny3 = foundAny3 + 1 end
+    return 4
+  end)
+  Even_PPPrint(hex_map, 10)
+  -- PPPrint(hex_map)
+  print("foundAny1 = "..foundAny1)  -- ### DEBUG
+  assert(foundAny1 == 5, "Expected value of 5, actually "..tostring(foundAny1))
+  print("foundAny2 = "..foundAny2)  -- ### DEBUG
+  assert(foundAny2 == 8, "Expected value of 8, actually "..tostring(foundAny2))
+  print("foundAny3 = "..foundAny3)  -- ### DEBUG
+  assert(foundAny3 == 8, "Expected value of 8, actually "..tostring(foundAny3))
+end
+
+function Test_Circle_All()
+  print("\n\nTest_Circle_All")
+  local hex_map = Setup_Hex_Map(100, 8)
+  hex_map:map(function (x) return 0 end)
+  local idx = 0
+  local foundAll1 = 0
+  local foundAll2 = 0
+  hex_map:gen(function (hx, x, newWave, dist, cir)
+    idx = idx + 1
+    if idx == 8 then return 8 end
+    if cir(1):all(function(v, bit_count, bit)
+      if bit_count(v) ~= 1 then return false end
+      if bit(v) == 4 then return true else return false end
+    end) then foundAll1 = foundAll1 + 1 end
+    return 4
+  end)
+  PPPrint(hex_map); print()
+  print("foundAll1 = "..foundAll1)  -- ### DEBUG
+  assert(foundAll1 == 36, "Expected value of 36, actually "..tostring(foundAll1))
 end
 
 function Test_Gen_NewWave_Selection()
@@ -233,7 +333,8 @@ function Test_Gen_NewWave_Selection()
     end
     return x
   end)
-  hex_map:pprint()
+  Even_PPPrint(hex_map, 10)
+  -- hex_map:pprint()
   assert(hex_map[34] == -2, "Expected value of -2, actually "..tostring(hex_map[34]))
   assert(hex_map[22] == -3, "Expected value of -3, actually "..tostring(hex_map[22]))
   assert(hex_map[56] == -5, "Expected value of -5, actually "..tostring(hex_map[56]))
@@ -275,5 +376,7 @@ Test_Bit_Count()
 Test_Circle_Placement()
 Test_HexMapHelper_function()
 Test_Circle()
+Test_Circle_Any()
+Test_Circle_All()
 Test_Gen_NewWave_Selection()
 Test_Parlous_WFC_Gen_Time()
