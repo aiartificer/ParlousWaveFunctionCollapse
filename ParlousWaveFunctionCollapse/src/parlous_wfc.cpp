af1156b9212702c9f4bbc91a723227b70457b708
aiartificer <sjfcannon@hotmail.com>
2026-03-19 18:40:25 -0400

    Debugging

--- ParlousWaveFunctionCollapse/src/parlous_wfc.cpp
+++ ParlousWaveFunctionCollapse/src/parlous_wfc.cpp
@@ -490,7 +490,7 @@ static int __updateDomainCall(lua_State* L,       //// [-0, +0, m]
     lua_call(L, 6, 1);                              // [-7, +1, e]
     T result = (T)luaL_checknumber(L, -1);
     hexMap[l] = ~result;
-    printf("###@@@ hexMap[%li] = %li", l, hexMap[l]);  // ### DEBUG
+    printf("###@@@ hexMap[%li] = %li\n", l, hexMap[l]);  // ### DEBUG
     lua_pop(L, 1);                                  // [-1, +0, -]
 
     return 0;
