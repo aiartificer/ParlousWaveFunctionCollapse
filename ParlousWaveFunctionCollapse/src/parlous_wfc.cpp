eaf0c647af0dab9edafd687215a8564ea9550c32
aiartificer <sjfcannon@hotmail.com>
2026-03-19 20:05:40 -0400

    Debugging

--- ParlousWaveFunctionCollapse/src/parlous_wfc.cpp
+++ ParlousWaveFunctionCollapse/src/parlous_wfc.cpp
@@ -491,7 +491,7 @@ static int __updateDomainCall(lua_State* L,       //// [-0, +0, m]
     T result = (T)luaL_checknumber(L, -1);
     hexMap[l] = ~result;
     // printf("###@@@ hexMap[%li] = %li\n", l, hexMap[l]);  // ### DEBUG
-    if (l == 119) printf("###@@@ hexMap[%li] = %li\n", l, hexMap[l]);  // ### DEBUG
+    if (l == 119) printf("###@@@ WAS SET hexMap[%li] = %li\n", l, hexMap[l]);  // ### DEBUG
     lua_pop(L, 1);                                  // [-1, +0, -]
 
     return 0;
